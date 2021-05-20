d172489cc5ad497c42d2285f00bc6eca26fdbbfc
juuuuu <66965172+2-juhyun-2@users.noreply.github.com>
2021-05-20 16:22:50 +0900

    Add files via upload

--- myproject/policy/models.py
+++ myproject/policy/models.py
@@ -7,7 +7,7 @@ class User(db.Model):
 	username = db.Column(db.String(150), unique=True, nullable=False)
 	password = db.Column(db.String(200), nullable=False)
 	email = db.Column(db.String(120), unique=True, nullable=False)
-	level = db.Column(db.String(120), nullable=False)
+	
 	
 
 	# def __init__(self, username, password):
